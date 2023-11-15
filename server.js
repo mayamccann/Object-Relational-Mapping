7eebb7823ffbfba0d73ff3632ec7965503cfec79
Maya McCann <mccannmaya@gmail.com>
2023-11-15 16:49:51 -0500

    sequelize code for connecting to server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const routes = require('./routes');
 
 const sequelize = require('./config/connection');
+
+
+// This is the code for syncing sequelize models to the database 
+// Then turn on the server is able
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+  });
