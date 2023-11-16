383fceb64623155ed2e269121df012bc539c8179
Maya McCann <mccannmaya@gmail.com>
2023-11-16 11:22:06 -0500

    id and tag_name for Tag.js for models

--- models/Tag.js
+++ models/Tag.js
@@ -6,8 +6,21 @@ class Tag extends Model {}
 
 Tag.init(
   {
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }, 
+
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+
     // define columns
   },
+  
   {
     sequelize,
     timestamps: false,
