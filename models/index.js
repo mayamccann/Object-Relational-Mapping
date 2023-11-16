eebeeebfd7b06824f11ebf49564468a3e6093bbf
Maya McCann <mccannmaya@gmail.com>
2023-11-16 10:17:01 -0500

    starter codes to models and routes files

--- models/index.js
+++ models/index.js
@@ -1,8 +1,20 @@
-// importing the models
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
+// Products belongsTo Category
 
+// Categories have many Products
 
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
-
+  Product,
+  Category,
+  Tag,
+  ProductTag,
 };
